0ba4a274232e9d64183c88e5ba3812f34b88e777
Frans <frans.trujillo@utec.edu.pe>
2024-10-09 23:41:51 -0500

    add mock server from enviroment variables

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -1,34 +1,22 @@
-"""
-TODO
-"""
-
+import os
 import requests
 
-BASE_URL = 'http://localhost:8000'
-
+#usaremos un mock server de postman
+BASE_URL=os.getenv('BASE_URL')
 
 def test_get_root():
-    """
-    TODO
-    """
 
     response = requests.get(f'{BASE_URL}/', timeout=30)
     assert response.status_code == 200
 
 
 def test_post_generate_dialog():
-    """
-    TODO
-    """
 
     response = requests.post(f'{BASE_URL}/generate/dialog', timeout=30)
     assert response.status_code == 201
 
 
 def test_post_generate_story():
-    """
-    TODO
-    """
 
     response = requests.post(f'{BASE_URL}/generate/story', timeout=30)
     assert response.status_code == 201
