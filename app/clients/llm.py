bf2b7d48821384f5a2b3ff73d12a2ed341777728
Frans <frans.trujillo@utec.edu.pe>
2024-10-14 20:55:16 -0500

    LLM return JSON, JSON is parsed and returned to client

--- app/clients/llm.py
+++ app/clients/llm.py
@@ -3,13 +3,17 @@ TODO
 """
 
 import os
-
+import json
 from dotenv import load_dotenv
 from openai import OpenAI, OpenAIError
 
 load_dotenv()
 
-API_KEY = os.getenv("OPENAI_API_KEY") or "null"
+API_KEY = os.getenv("API_KEY_OPENAI")
+
+if API_KEY is None:
+    print("Error: La variable API_KEY_OPENAI no se ha cargado.")
+
 
 client = OpenAI(api_key=API_KEY)
 
@@ -36,8 +40,21 @@ async def make_request(prompt: dict, model: str = "gpt-4o-mini"):
             n=1,
             temperature=0.7
         )
+        response=response.choices[0].message.content
 
-        return response.choices[0].message.content
+        try:
+            if response[:3]=="```":
+                response=response[7:-3]
+
+            response_json = json.loads(response)
+            return response_json
+
+        except json.JSONDecodeError as e:
+            print("Error al decodificar JSON:", e)
+
+
+        return response
+
     except OpenAIError as error:
-        print(error)
+        print(f'ocurrio un error en la llamada a OpenAI: ${error}')
         return None
--- app/prompts/story.py
+++ app/prompts/story.py
@@ -16,6 +16,69 @@ def generate_story_prompt(json):
         char_attrs = ", ".join([f"{x['name']}: {x['value']}" for x in character['attributes']])
         prompt += f"{character['name']} ({character['name'][0]}): {char_attrs}. "
 
-    prompt += "Generate a engaging story. Use character initials. Use plain text. No notes."
+    prompt += "Generate a engaging story. Use character initials. Use plain text. No notes.Return JSON"
 
     return prompt
+
+
+"""json_request={
+  "title": "Echoes of Destiny",
+  "settings": {
+    "size": "short",
+    "attributes": [
+      {
+        "name": "environment",
+        "value": "lush forests and towering mountains"
+      },
+      {
+        "name": "time period",
+        "value": "futuristic with ancient ruins"
+      }
+    ]
+  },
+  "characters": [
+    {
+      "name": "Alex",
+      "attributes": [
+        {
+          "name": "age",
+          "value": "18"
+        },
+        {
+          "name": "ability",
+          "value": "telekinesis"
+        }
+      ]
+    },
+    {
+      "name": "Luna",
+      "attributes": [
+        {
+          "name": "age",
+          "value": "20"
+        },
+        {
+          "name": "ability",
+          "value": "illusion"
+        }
+      ]
+    },
+    {
+      "name": "Draco",
+      "attributes": [
+        {
+          "name": "age",
+          "value": "35"
+        },
+        {
+          "name": "ability",
+          "value": "fire"
+        }
+      ]
+    }
+  ],
+  "plots": 1,
+"endings":1}
+
+response=generate_story_prompt(json_request)
+print(response)"""
